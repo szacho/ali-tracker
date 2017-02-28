37efed6b38eb125ea014f61cdd5f08cf10cbc098
szacho <whywayaway@gmail.com>
2017-02-28 19:30:06 +0100

    Set up the client-side of app

---
+++ client/src/components/app.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>The competition has begun.</div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import App from './components/app';
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
